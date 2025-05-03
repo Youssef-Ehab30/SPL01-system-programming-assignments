d05c75a0a6d1489225dc811278d3e6d95eabefbd
Youssef-Ehab30 <167027905+Youssef-Ehab30@users.noreply.github.com>
2025-05-03 05:15:36 +0300

    Create femtoShell.c

---
+++ shell-implementations/femtoShell.c
@@ -0,0 +1,54 @@
+#include <stdio.h>		// to use printf, perror, fgets
+#include <stdlib.h>		// to use exit
+#include <string.h>		// to use strcspn, strtok
+
+#define BUFFER_SIZE 10240
+#define READ_ERROR -1
+
+int main(int argc, char* argv[]) {
+	char input[BUFFER_SIZE];
+
+	while (1) {
+		printf("%s$ ", argv[0]);
+		if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+			if (feof(stdin)) {
+				printf("\n"); // mimic shell behavior on EOF
+				break;
+			}
+			else {
+				perror("Error reading input");
+				exit(READ_ERROR);
+			}
+		}
+
+		// remove trailing newline
+		input[strcspn(input, "\n")] = 0;
+
+		char* command = strtok(input, " "); // split using space into tokens
+		if (command == NULL) {
+			continue; // empty input
+		}
+
+		if (strcmp(command, "exit") == 0) {
+			printf("Good Bye\n");
+			break;
+		}
+		else if (strcmp(command, "echo") == 0) {
+			char* argument = strtok(NULL, " "); // get next token
+			while (argument != NULL) {
+				printf("%s", argument);
+				char* next_argument = strtok(NULL, " "); // get next token
+				if (next_argument != NULL) {
+					printf(" ");
+				}
+				argument = next_argument;
+			}
+			printf("\n");
+		}
+		else {
+			printf("Invalid command\n");
+		}
+	}
+
+	return 0;
+}
